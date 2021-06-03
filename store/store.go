a790d93e526d9b3fba2c8daa217882e463381f7e
AngelDhakal <anjaldhakal01@gmail.com>
2021-06-03 22:00:59 +0545

    store object stores the repository methods for all models and returns through NewStore() function

--- store/store.go
+++ store/store.go
@@ -10,17 +10,13 @@ type Store struct {
 	conn *sql.DB
 }
 
-func NewStore() Store {
-	return Store{
+func NewStore() Storer {
+	return &Store{
 		conn: models.Connect(),
 	}
 }
 
 type Storer interface {
-	GetUser(int64) (models.Users, error)
-	GetUserByEmail(string) (models.Users, error)
-	ListUsers() ([]models.Users, error)
-	CreateUser(CreateUserParams) (models.Users, error)
-	UpdateUser(UpdateUserParams) (models.Users, error)
-	DeleteUser(int64) error
+	UserStorer
+	TokenStorer
 }
