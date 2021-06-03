08c1caa1ad1171fc0878cefa8857ceb2f9c5ff1d
AngelDhakal <anjaldhakal01@gmail.com>
2021-06-03 22:43:47 +0545

    deleted

--- schema/users_schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS users (
-    id BIGSERIAL PRIMARY KEY,
-    created_at TEXT NOT NULL,
-    updated_at TEXT,
-    email TEXT NOT NULL UNIQUE,
-    username TEXT NOT NULL,
-    password TEXT NOT NULL
-);
