7791a4c7c7442931275f5488cb2a4a9b94e11506
AngelDhakal <anjaldhakal01@gmail.com>
2021-06-04 01:22:24 +0545

    unix time validator function removed

--- util/dates.go
+++ util/dates.go
@@ -9,7 +9,3 @@ func GetCurrentDate() string {
 	date := currentTime.Format("2006-01-02 15:04:05")
 	return date
 }
-
-func IsTimeExpired(unixTime int64) bool {
-	return unixTime < time.Now().Unix()
-}
