d645280d4638ffd4b8433232d500d0021cf19d6b
AngelDhakal <anjaldhakal01@gmail.com>
2021-06-11 13:50:37 +0545

    Movies struct created for movies table

--- models/models.go
+++ models/models.go
@@ -2,6 +2,15 @@ package models
 
 import (
 	"database/sql"
+	"time"
+)
+
+type Status string
+
+const (
+	Completed Status = "c"
+	Waiting   Status = "w"
+	Plan      Status = "p"
 )
 
 type Tokens struct {
@@ -19,4 +28,17 @@ type Users struct {
 	Email     string         `json:"email"`
 	Username  string         `json:"username"`
 	Password  string         `json:"password"`
+}
+
+type Movies struct {
+	ID            int64          `json:"id"`
+	CreatedAt     string         `json:"created_at"`
+	UpdatedAt     sql.NullString `json:"updated_at"`
+	UserID        int64          `json:"user_id"`
+	Name          string         `json:"name"`
+	Status        Status         `json:"status"`
+	CurrentLength int64          `json:"current_length"`
+	Year          time.Time      `json:"year"`
+	Language      string         `json:"language"`
+	TotalLength   int64          `json:"total_length"`
 }
