24dc3f8aa99c88c034fd92b5dc08609781a10a02
AngelDhakal <anjaldhakal01@gmail.com>
2021-06-11 15:57:00 +0545

    default values added for some columns & total_length and current_length column data type changed to BIGINT

--- models/schemas.go
+++ models/schemas.go
@@ -3,19 +3,19 @@ package models
 var usersTable = `
 CREATE TABLE IF NOT EXISTS users (
     id BIGSERIAL PRIMARY KEY,
-    created_at VARCHAR(55) NOT NULL,
-    updated_at VARCHAR(55),
+    created_at TEXT NOT NULL,
+    updated_at TEXT,
     email VARCHAR(320) NOT NULL UNIQUE,
-    username VARCHAR(55) NOT NULL,
-    password VARCHAR(55) NOT NULL
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
 );
 `
 
 const tokensTable = `
 CREATE TABLE IF NOT EXISTS tokens (
     id BIGSERIAL NOT NULL,
-    created_at VARCHAR(55) NOT NULL,
-    updated_at VARCHAR(55),
+    created_at TEXT NOT NULL,
+    updated_at TEXT,
     token VARCHAR(32) UNIQUE NOT NULL,
     user_id BIGINT UNIQUE NOT NULL,
     CONSTRAINT tokens_pk PRIMARY KEY (id),
@@ -27,18 +27,14 @@ const moviesTable = `
 CREATE TABLE IF NOT EXISTS movies (
     id BIGSERIAL PRIMARY KEY,
     user_id BIGSERIAL NOT NULL,
-    created_at VARCHAR(55) NOT NULL,
-    updated_at VARCHAR(55),
+    created_at TEXT NOT NULL,
+    updated_at TEXT,
     name VARCHAR(255) NOT NULL,
-    status STATUS NOT NULL,
-    current_length INTEGER,
-    year DATE,
-    total_length INTEGER,
-    language VARCHAR(55),
+    status VARCHAR(1) NOT NULL,
+    current_length BIGINT DEFAULT 0,
+    year DATE DEFAULT '1970-01-01',
+    total_length BIGINT DEFAULT 0,
+    language VARCHAR(255) DEFAULT 'english',
     CONSTRAINT users_movie_fk FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 `
-
-// const statusType = `
-//     CREATE TYPE STATUS AS ENUM ('c','w','p');
-// `
