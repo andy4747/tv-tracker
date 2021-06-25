7298190443aae93f3570cd34bf8fb3e5c8244c34
angeldhakal <anjaldhakal01@gmail.com>
2021-06-25 15:47:11 +0545

    added Switch inside BrowserRouter

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,22 @@
 import React, { FC } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import { Home } from './Components/Auth/Home';
+import Login from './Components/Auth/Login';
+import { SignUp } from './Components/Auth/SignUp';
+import { Dashboard } from './Components/Private/Dashboard';
+import { PrivateRoute } from './Utils/PrivateRouter';
 
 export const App: FC = () => {
 	return (
 		<>
 			<Router>
-				<Route path='/' exact component={Home} />
-				<Route path='/signup' exact component={Home} />
-				<Route path='/login' exact component={Home} />
-				{/* <Route path='/' component={Home} /> */}
+				<Switch>
+					<Route path='/' exact component={Home} />
+					<Route path='/signup' exact component={SignUp} />
+					<Route path='/login' exact component={Login} />
+					<PrivateRoute path='/dashboard' exact component={Dashboard} />
+				</Switch>
 			</Router>
 		</>
 	);
