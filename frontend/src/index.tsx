fdad5ddfef355f9bba2674786d6cd6b8f39ba2b6
angeldhakal <anjaldhakal01@gmail.com>
2021-06-25 15:56:27 +0545

    used ThemeProvider to apply new custom theme

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,10 +1,14 @@
+import { ThemeProvider } from '@material-ui/core/styles';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
+import { theme } from './theme';
 
 ReactDOM.render(
 	<React.StrictMode>
-		<App />
+		<ThemeProvider theme={theme}>
+			<App />
+		</ThemeProvider>
 	</React.StrictMode>,
 	document.getElementById('root')
 );
