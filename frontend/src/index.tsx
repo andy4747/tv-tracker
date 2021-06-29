33037167e9c8c4c4a7c51d7acad0ad47d295bfe3
angeldhakal <anjaldhakal01@gmail.com>
2021-06-29 16:36:39 +0545

    strict mode removed

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,13 +2,13 @@ import { ThemeProvider } from '@material-ui/core/styles';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
-import { theme } from './theme';
+import { theme } from './Theme';
 
 ReactDOM.render(
-	<React.StrictMode>
-		<ThemeProvider theme={theme}>
-			<App />
-		</ThemeProvider>
-	</React.StrictMode>,
+	// <React.StrictMode>
+	<ThemeProvider theme={theme}>
+		<App />
+	</ThemeProvider>,
+	// </React.StrictMode>,
 	document.getElementById('root')
 );
