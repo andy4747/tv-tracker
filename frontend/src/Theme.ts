a0b68888013d532c13aea5983f58344d271cb51e
angeldhakal <anjaldhakal01@gmail.com>
2021-06-29 16:44:07 +0545

    renamed from theme.ts to Theme.ts

---
+++ frontend/src/Theme.ts
@@ -0,0 +1,12 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+export const theme = createMuiTheme({
+	palette: {
+		primary: {
+			main: '#D81B60',
+		},
+		secondary: {
+			main: '#212121',
+		},
+	},
+});
